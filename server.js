34adc8c98ac0023dbec8f39754f17c5ffc1c7581
fortuneeffiong123 <fortuneeffiong123@gmail.com>
2026-05-22 09:53:00 -0500

    Initial Node.js application

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Node.js App Successfully Deployed!</h1>
+    <p>GitHub Actions CI/CD is working.</p>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
